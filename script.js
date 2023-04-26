313e9513e9662b64f861d115441a291a77d70602
AshIsh <33283957+Ashish-Jewargikar@users.noreply.github.com>
2023-04-26 23:18:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 // your code here
+const form = document.getElementById('myForm');
+    const button = document.getElementById('button');
+    const url = document.getElementById('url');
+
+    form.addEventListener('submit', function(event) {
+      event.preventDefault();
+      const nameValue = document.getElementById('name').value;
+      const yearValue = document.getElementById('year').value;
+      const queryString = `?name=${nameValue}&year=${yearValue}`;
+      url.textContent = `https://localhost:8080/${queryString}`;
+    });
